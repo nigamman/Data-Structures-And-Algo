d97de72b190b593d096f78ac034452555ef4a33b
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-04-13 15:07:07 +0530

    added

--- Heaps/heap_STL.cpp
+++ Heaps/heap_STL.cpp
@@ -11,13 +11,10 @@ int main () {
     maxHeap.push(20);
     maxHeap.push(30);
 
-    cout << maxHeap.top() << endl;
-
+    cout << "top element of Max Heap: " << maxHeap.top() << endl;
     maxHeap.pop();
-
-    cout << maxHeap.top() << endl;
-
-    cout << maxHeap.size() << endl;
+    cout << "top element of Max Heap: " << maxHeap.top() << endl;
+    cout << "Size of Max Heap: " <<maxHeap.size() << endl;
 
     if(maxHeap.empty()) {
         cout << "Empty" << endl;
@@ -25,4 +22,32 @@ int main () {
     else {
         cout << "Non Empty" << endl;
     }
+
+    //min-heap
+    priority_queue<int,vector<int>, greater<int> > pq;
+    pq.push(3);
+    pq.push(6);
+    pq.push(9);
+    pq.push(4);
+    pq.push(8);
+    
+    cout << "top element of Min Heap: " << pq.top() << endl;
+    pq.pop();
+        cout << "Size: " << pq.size() << endl;
+        cout << "top element of Min Heap: " << pq.top() << endl;
+    pq.pop();
+        cout << "top element of Min Heap: " << pq.top() << endl;
+    pq.pop();
+        cout << "top element of Min Heap: " << pq.top() << endl;
+    pq.pop();
+        cout << "top element of Min Heap: " << pq.top() << endl;
+    pq.pop();
+    
+    if(pq.empty()) {
+        cout << "Min heap is empty" << endl;
+    }
+    else {
+        cout << "Min heap is not empty" << endl;
+    }
+    return 0;
 }
--- Heaps/heaps_class-2.cpp
+++ Heaps/heaps_class-2.cpp
@@ -2,10 +2,28 @@
 #include <queue>
 using namespace std;
 
+int kthSmallest (int arr[], int l, int r, int k) {
+    
+    priority_queue<int>maxHeap;
 
+    //step1- first push k elements in the maxHeap
+    for(int i=l; i<k; i++) {
+        maxHeap.push(arr[i]);
+    }
+
+    //step2- after pushing k elements, comparing the values of max heap top element and arr[i] element and push the smallest element
+    for(int i=k; i<=r; i++) {
+        if(maxHeap.top() > arr[i]) {
+            maxHeap.pop();
+            maxHeap.push(arr[i]);
+        }
+    }
+    return maxHeap.top();
+}
 
 int main () {
 
-    return 0;
+   int arr[] = {12,34,5,2,12,43,1};
+   
 
 }
